7241be7e14f6037ade6091ba911ce9acb14ff670
anlingbbq <lijie1992no1@163.com>
2017-03-05 20:34:13 +0800

    加入dotween的动画

--- Assets/Scripts/Panel/SystemPanel.cs
+++ Assets/Scripts/Panel/SystemPanel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,13 +9,21 @@ public class SystemPanel : UIBasePanel {
         if (m_CanvasGroup == null)
             m_CanvasGroup = GetComponent<CanvasGroup>();
 
-        m_CanvasGroup.alpha = 1;
+        m_CanvasGroup.alpha = 0;
         m_CanvasGroup.blocksRaycasts = true;
+
+        Vector3 temp = transform.localPosition;
+        temp.y = -Screen.height / 2.0f;
+        transform.localPosition = temp;
+        transform.DOLocalMoveY(0, 0.5f);
+        m_CanvasGroup.DOFade(1, 0.5f);
     }
 
     public override void OnExit()
     {
-        m_CanvasGroup.alpha = 0;
         m_CanvasGroup.blocksRaycasts = false;
+
+        transform.DOLocalMoveY(-Screen.width / 2.0f, 0.5f);
+        m_CanvasGroup.DOFade(0, 0.5f);
     }
 }
--- Assets/Scripts/Panel/TaskPanel.cs
+++ Assets/Scripts/Panel/TaskPanel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class TaskPanel : UIBasePanel
@@ -9,13 +10,21 @@ public class TaskPanel : UIBasePanel
         if (m_CanvasGroup == null)
             m_CanvasGroup = GetComponent<CanvasGroup>();
 
-        m_CanvasGroup.alpha = 1;
+        m_CanvasGroup.alpha = 0;
         m_CanvasGroup.blocksRaycasts = true;
+
+        Vector3 temp = transform.localPosition;
+        temp.y = -Screen.height / 2.0f;
+        transform.localPosition = temp;
+        transform.DOLocalMoveY(0, 0.5f);
+        m_CanvasGroup.DOFade(1, 0.5f);
     }
 
     public override void OnExit()
     {
-        m_CanvasGroup.alpha = 0;
         m_CanvasGroup.blocksRaycasts = false;
+
+        transform.DOLocalMoveY(-Screen.width/2.0f, 0.5f);
+        m_CanvasGroup.DOFade(0, 0.5f);
     }
 }
